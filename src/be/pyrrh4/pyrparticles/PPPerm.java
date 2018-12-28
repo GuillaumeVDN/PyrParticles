1e4cabd51f42c5fe6b1e81351d2d0d099e689156
PYRRH4 <PYRRH4@users.noreply.github.com>
2018-12-28 14:34:04 +0100

    PyrParticles v5.0.1-BETA

--- src/be/pyrrh4/pyrparticles/PPPerm.java
+++ src/be/pyrrh4/pyrparticles/PPPerm.java
@@ -4,11 +4,11 @@ import be.pyrrh4.pyrcore.lib.Perm;
 
 public class PPPerm {
 
-	public static final Perm PYRPARTICLES_ROOT = new Perm("PYRPARTICLES_ROOT", null, "pyrparticles");
-	public static final Perm PYRPARTICLES_ADMIN = new Perm("PYRPARTICLES_ADMIN", PYRPARTICLES_ROOT, "admin");
-	public static final Perm PYRPARTICLES_COMMAND_ROOT = new Perm("PYRPARTICLES_COMMAND_ROOT", PYRPARTICLES_ROOT, "command");
-	public static final Perm PYRPARTICLES_COMMAND_GADGET = new Perm("PYRPARTICLES_COMMAND_GADGET", PYRPARTICLES_COMMAND_ROOT, "gadget");
-	public static final Perm PYRPARTICLES_COMMAND_PARTICLE = new Perm("PYRPARTICLES_COMMAND_PARTICLE", PYRPARTICLES_COMMAND_ROOT, "particle");
-	public static final Perm PYRPARTICLES_COMMAND_TRAIL = new Perm("PYRPARTICLES_COMMAND_TRAIL", PYRPARTICLES_COMMAND_ROOT, "trail");
+	public static final Perm PYRPARTICLES_ROOT = new Perm(null, "pyrparticles.*");
+	public static final Perm PYRPARTICLES_ADMIN = new Perm(PYRPARTICLES_ROOT, "pyrparticles.admin");
+	public static final Perm PYRPARTICLES_COMMAND_ROOT = new Perm(PYRPARTICLES_ROOT, "pyrparticles.command.*");
+	public static final Perm PYRPARTICLES_COMMAND_GADGET = new Perm(PYRPARTICLES_COMMAND_ROOT, "pyrparticles.command.gadget");
+	public static final Perm PYRPARTICLES_COMMAND_PARTICLE = new Perm(PYRPARTICLES_COMMAND_ROOT, "pyrparticles.command.particle");
+	public static final Perm PYRPARTICLES_COMMAND_TRAIL = new Perm(PYRPARTICLES_COMMAND_ROOT, "pyrparticles.command.trail");
 
 }
